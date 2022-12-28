49a6f18853187ea38832cf3203c4d5cb612eee86
manato kuroda <a7108100@gmail.com>
2022-12-28 16:53:47 +0900

    feat: set up nuxt

---
+++ packages/nuxt/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+      <ul class="flex flex-wrap -mb-px">
+        <li class="mr-2">
+          <NuxtLink to="/" class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300">Home</NuxtLink>
+        </li>
+        <li class="mr-2">
+          <NuxtLink to="/about" class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300">About</NuxtLink>
+        </li>
+        <li class="mr-2">
+          <NuxtLink to="/works" class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300">Works</NuxtLink>
+        </li>
+        <li class="mr-2">
+          <NuxtLink to="/blog" class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300">Blog</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="p-4">
+      <slot />  
+    </div>
+  </div>
+
+</template>
---
+++ packages/nuxt/pages/works.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hello, {{title}} from Nuxt</h1>
+</template>
+
+<script setup lang="ts">
+const title = 'Works'
+
+</script>
